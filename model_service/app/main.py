44a8dd89ffd33280deb51a0c136d8f46cc30167f
askarovi <askarovi@kolesa.kz>
2026-03-30 22:46:07 +0500

    запускаю веса при старте контейнера

--- model_service/app/main.py
+++ model_service/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.routes.inference import router as inference_router
+from app.services.predictor import _load_model
 
 app = FastAPI(
     title="Model Service",
@@ -7,6 +8,11 @@ app = FastAPI(
     redoc_url=None,      # Отключение ReDoc
     openapi_url=None     # Отключение OpenAPI схемы
 )
+
+@app.on_event("startup")
+async def startup_event():
+    _load_model()
+    print("[model_service] Модель успешно загружена при старте")
 
 app.include_router(inference_router)
 
